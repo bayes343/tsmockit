7690bb4750c7e7db562ad23aa89cd8711a149f59
Joey Bayes <joey.bayes@uscca.com>
2022-07-30 23:11:01 -0400

    Remove test helpers (static) class.

---
+++ src/Utility/tests/EmitEventAtElement.spec.ts
@@ -0,0 +1,16 @@
+import { EmitEventAtElement } from '../EmitEventAtElement';
+import { Expect } from '../Expect';
+
+describe('EmitEventAtElement', () => {
+  it('should emit event at element', async () => {
+    let eventFired = false;
+    const testElement = document.createElement('button');
+    testElement.addEventListener('click', () => eventFired = true);
+
+    EmitEventAtElement(testElement, 'click');
+
+    await Expect(
+      () => eventFired,
+      (m) => m.toBeTruthy());
+  });
+});
